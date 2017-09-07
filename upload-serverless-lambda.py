53155073080f199439ac495824ae18fa38cb8d76
Ariane Gadd <ariane.gadd@gmail.com>
2017-09-07 15:41:57 +0100

    add python script

---
+++ upload-serverless-lambda.py
@@ -0,0 +1,20 @@
+import boto3
+import io
+from io import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+serverless_bucket = s3.Bucket('ariane-serverless')
+build_bucket = s3.Bucket('ariane-serverless-build')
+
+serverless_zip = io.BytesIO()
+build_bucket.download_fileobj('serverlessbuild.zip', serverless_zip)
+
+with zipfile.ZipFile(serverless_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        serverless_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
+        serverless_bucket.Object(nm).Acl().put(ACL='public-read')
