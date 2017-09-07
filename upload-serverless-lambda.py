68797294f6f7640ed5f3e4f635d20ca6bd7d8ee4
Ariane Gadd <ariane.gadd@gmail.com>
2017-09-07 16:02:42 +0100

    update python for lambda

--- upload-serverless-lambda.py
+++ upload-serverless-lambda.py
@@ -4,17 +4,20 @@ from io import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-serverless_bucket = s3.Bucket('ariane-serverless')
-build_bucket = s3.Bucket('ariane-serverless-build')
+    serverless_bucket = s3.Bucket('ariane-serverless')
+    build_bucket = s3.Bucket('ariane-serverless-build')
 
-serverless_zip = io.BytesIO()
-build_bucket.download_fileobj('serverlessbuild.zip', serverless_zip)
+    serverless_zip = io.BytesIO()
+    build_bucket.download_fileobj('serverlessbuild.zip', serverless_zip)
 
-with zipfile.ZipFile(serverless_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        serverless_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
-        serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(serverless_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            serverless_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
+            serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+    return 'Hello from Lambda'
