e369b57bf35fda0ff4bd49bb09458ec17d42908e
Ariane Gadd <ariane.gadd@gmail.com>
2017-09-08 19:44:56 +0100

    seb is now aweswome

--- upload-serverless-lambda.py
+++ upload-serverless-lambda.py
@@ -1,3 +1,5 @@
+#update lambda permissions to allow to print to cloudwatch logs.
+
 import boto3
 import io
 from io import StringIO
@@ -8,27 +10,44 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:eu-west-1:226342546049:ariane-serverless-topic')
     
+    location = {
+        "bucketName": 'ariane-serverless-build',
+        "objectKey": 'serverlessbuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+        
         s3 = boto3.resource('s3')
         
         serverless_bucket = s3.Bucket('ariane-serverless')
-        build_bucket = s3.Bucket('ariane-serverless-build')
+        build_bucket = s3.Bucket(location["bucketName"])
     
-        serverless_zip = io.BytesIO()
-        build_bucket.download_fileobj('serverlessbuild.zip', serverless_zip)
+        serverless_zip = io.BytesIO() #had to use Bytes rather than String as using Python3. What is StringIO for?
+        build_bucket.download_fileobj(location["objectKey"], serverless_zip)
     
         with zipfile.ZipFile(serverless_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
                 serverless_bucket.upload_fileobj(obj, nm,
-                    ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
-                serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]}) #what is this bit for?
+                serverless_bucket.Object(nm).Acl().put(ACL='public-read') #makes the bucket public
         
         topic.publish(Subject="Serverless deployed", Message="Serverless deployed successfully!")
-    
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+        
     except:
         topic.publish(Subject="Serverless Deploy Failed", Message="The serverless was not deployed successfully.")
         raise
+
+    print("Job done!")
     
     return 'Hello from Lambda'
+    
     
